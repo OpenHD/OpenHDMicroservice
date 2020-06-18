085c7d17a0affcc0d09829eb54b3995c6c87d752
Stephen Oliver <steve@infincia.com>
2020-06-18 13:46:56 -0400

    Push tagged releases to cloudsmith

--- package.sh
+++ package.sh
@@ -33,3 +33,15 @@ fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION//v} -C ${TM
   -d "libboost-system-dev >= 1.62.0" \
   -d "libboost-program-options-dev >= 1.62.0" \
   -d "openhd-router >= 0.1.3" || exit 1
+
+#
+# Only push to cloudsmith for tags. If you don't want something to be pushed to the repo, 
+# don't create a tag. You can build packages and test them locally without tagging.
+#
+git describe --exact-match HEAD > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+    echo "Pushing package to OpenHD repository"
+    cloudsmith push deb openhd/openhd/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb
+else
+    echo "Not a tagged release, skipping push to OpenHD repository"
+fi
