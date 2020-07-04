04adb6933ede2fd358870070318cec3bad272aa3
Stephen Oliver <steve@infincia.com>
2020-07-04 14:25:21 -0400

    Install lifepoweredpi package before building

--- package.sh
+++ package.sh
@@ -10,7 +10,16 @@ if [[ "${PLATFORM}" == "pi" ]]; then
     PACKAGE_ARCH="armhf"
 fi
 
-apt -y install libasio-dev libboost-system-dev libboost-program-options-dev
+
+apt-get install -y apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/openhd/openhd-2-0/cfg/gpg/gpg.B9F0E99CF5787237.key' | apt-key add -
+
+
+echo "deb https://dl.cloudsmith.io/public/openhd/openhd-2-0/deb/${OS} ${DISTRO} main" > /etc/apt/sources.list.d/openhd-2-0.list
+
+apt -y update
+
+apt -y install libasio-dev libboost-system-dev libboost-program-options-dev lifepoweredpi
 
 PACKAGE_NAME=openhd-microservice
 
