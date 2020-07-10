1f029e0082386f1187d06ee915a1c071cca8bc4c
Stephen Oliver <steve@infincia.com>
2020-07-10 14:09:36 -0400

    Update mavlink submodule to openhd4

--- lib/mavlink_generated/include/mavlink/v2.0/openhd/testsuite.h
+++ lib/mavlink_generated/include/mavlink/v2.0/openhd/testsuite.h
@@ -496,6 +496,121 @@ static void mavlink_test_openhd_gpio_state(uint8_t system_id, uint8_t component_
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
+static void mavlink_test_openhd_ground_orientation(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_OPENHD_GROUND_ORIENTATION >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_openhd_ground_orientation_t packet_in = {
+        17235,17339,17,84
+    };
+    mavlink_openhd_ground_orientation_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.heading = packet_in.heading;
+        packet1.elevation = packet_in.elevation;
+        packet1.target_system = packet_in.target_system;
+        packet1.target_component = packet_in.target_component;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_OPENHD_GROUND_ORIENTATION_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_OPENHD_GROUND_ORIENTATION_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_openhd_ground_orientation_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_openhd_ground_orientation_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_openhd_ground_orientation_pack(system_id, component_id, &msg , packet1.target_system , packet1.target_component , packet1.heading , packet1.elevation );
+    mavlink_msg_openhd_ground_orientation_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_openhd_ground_orientation_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.target_system , packet1.target_component , packet1.heading , packet1.elevation );
+    mavlink_msg_openhd_ground_orientation_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_openhd_ground_orientation_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_openhd_ground_orientation_send(MAVLINK_COMM_1 , packet1.target_system , packet1.target_component , packet1.heading , packet1.elevation );
+    mavlink_msg_openhd_ground_orientation_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
+static void mavlink_test_openhd_head_orientation(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_OPENHD_HEAD_ORIENTATION >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_openhd_head_orientation_t packet_in = {
+        17235,17339,17443,151,218
+    };
+    mavlink_openhd_head_orientation_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.pitch = packet_in.pitch;
+        packet1.roll = packet_in.roll;
+        packet1.yaw = packet_in.yaw;
+        packet1.target_system = packet_in.target_system;
+        packet1.target_component = packet_in.target_component;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_OPENHD_HEAD_ORIENTATION_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_OPENHD_HEAD_ORIENTATION_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_openhd_head_orientation_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_openhd_head_orientation_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_openhd_head_orientation_pack(system_id, component_id, &msg , packet1.target_system , packet1.target_component , packet1.pitch , packet1.roll , packet1.yaw );
+    mavlink_msg_openhd_head_orientation_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_openhd_head_orientation_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.target_system , packet1.target_component , packet1.pitch , packet1.roll , packet1.yaw );
+    mavlink_msg_openhd_head_orientation_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_openhd_head_orientation_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_openhd_head_orientation_send(MAVLINK_COMM_1 , packet1.target_system , packet1.target_component , packet1.pitch , packet1.roll , packet1.yaw );
+    mavlink_msg_openhd_head_orientation_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_openhd_status_message(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
@@ -620,6 +735,8 @@ static void mavlink_test_openhd(uint8_t system_id, uint8_t component_id, mavlink
     mavlink_test_openhd_ground_load(system_id, component_id, last_msg);
     mavlink_test_openhd_ground_power(system_id, component_id, last_msg);
     mavlink_test_openhd_gpio_state(system_id, component_id, last_msg);
+    mavlink_test_openhd_ground_orientation(system_id, component_id, last_msg);
+    mavlink_test_openhd_head_orientation(system_id, component_id, last_msg);
     mavlink_test_openhd_status_message(system_id, component_id, last_msg);
     mavlink_test_openhd_version_message(system_id, component_id, last_msg);
 }
