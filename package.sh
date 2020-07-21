061a3142a559db321cbfbe1a7914549be9a140b1
Stephen Oliver <steve@infincia.com>
2020-07-21 00:15:41 -0400

    Bump openhd-router version requirement

--- package.sh
+++ package.sh
@@ -51,7 +51,7 @@ fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION//v} -C ${TM
   -d "libboost-filesystem-dev >= 1.62.0" \
   -d "libboost-system-dev >= 1.62.0" \
   -d "libboost-program-options-dev >= 1.62.0" \
-  -d "openhd-router >= 0.1.3" || exit 1
+  -d "openhd-router >= 0.1.8" || exit 1
 
 #
 # Only push to cloudsmith for tags. If you don't want something to be pushed to the repo, 
